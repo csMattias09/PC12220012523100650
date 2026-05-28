e77c07b0a1292f070b37c0c5fd569607eb53e8c7
csMattias09 <22200125@ue.edu.pe>
2026-05-28 18:22:57 -0500

    Agregar archivos de proyecto.

---
+++ PC12220012523100650.CORE/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace PC12220012523100650.CORE
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ PC12220012523100650.COREE/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace PC12220012523100650.COREE
+{
+    public class Class1
+    {
+
+    }
+}
